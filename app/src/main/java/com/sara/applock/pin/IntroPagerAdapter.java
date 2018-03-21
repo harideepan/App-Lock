c5cabf2aa15487779a0c29978705c0d6dcb1f278
Hariharan <hariharanmdvn@gmail.com>
2018-03-21 11:53:58 +0530

    Initial commit

---
+++ app/src/main/java/com/sara/applock/pin/IntroPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.sara.applock.pin;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.sara.applock.R;
+
+
+/**
+ * Created by Hariharan on 09-06-2017.
+ */
+
+
+public class IntroPagerAdapter extends FragmentPagerAdapter {
+    int[] img={R.drawable.screen0,R.drawable.screen1,R.drawable.screen2};
+
+    public IntroPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return ImageFragment.newInstance(img[position],position,img.length);
+    }
+    @Override
+    public int getCount() {
+        return img.length;
+    }
+}
+
+
